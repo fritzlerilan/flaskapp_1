aa979f21df59e8096ba523b34c07b94d06decfd7
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-01-04 20:20:20 -0300

    create endpoint /people GET with params

--- app.py
+++ app.py
@@ -54,31 +54,51 @@ def info():
     })
 
 
-@app.route('/people', methods=['POST'])
+@app.route('/people', methods=['POST', 'GET'])
 def people():
-    req_data = request.get_json()
-    name = None
-    dni = None
-    height = None
-    if 'name' in req_data and 'dni' in req_data and 'height' in req_data:
-        name = req_data['name']
-        dni = req_data['dni']
-        height = req_data['height']
-        if type(name) == str and type(dni) == int and type(height) == float:
-            if people_exist(dni):
-                return 'a person with dni {} already exists in the system'.format(dni), 409
-            else:
-                people_list.append(req_data)
-                return jsonify({
-                    "messege": "success"
-                }), 201
-    return {
-        "messege": "400 - BAD REQUEST",
-        "expected": {
-            "name": "<str value>",
-            "dni": "<int value>",
-            "height": "<float value>" 
-        }
-    }, 400
+    if request.method == 'POST':
+
+        req_data = request.get_json()
+        name = None
+        dni = None
+        height = None
+        if 'name' in req_data and 'dni' in req_data and 'height' in req_data:
+            name = req_data['name']
+            dni = req_data['dni']
+            height = req_data['height']
+            if type(name) == str and type(dni) == int and type(height) == float:
+                if people_exist(dni):
+                    return 'a person with dni {} already exists in the system'.format(dni), 409
+                else:
+                    people_list.append(req_data)
+                    return jsonify({
+                        "messege": "success"
+                    }), 201
+        return {
+            "messege": "400 - BAD REQUEST",
+            "expected": {
+                "name": "<str value>",
+                "dni": "<int value>",
+                "height": "<float value>" 
+            }
+        }, 400
+    
+
+    if request.method == 'GET':
+        
+        args = request.args.get('dni', None)
+        
+        if args is not None:
+            dni = int(args)
+            for ppl in people_list:
+                if ppl['dni'] == dni:
+                    return jsonify(ppl), 200
+        
+            return '', 204
+        
+        return jsonify(people_list), 200
+
+
+
 if __name__ == '__main__':
     app.run(debug = True)
