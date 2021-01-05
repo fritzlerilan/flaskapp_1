3bec07e973d44d93e9f4c48448a34b644bdda79b
fritzlerilan <fritzlerilan@gmail.com>
2021-01-05 12:51:03 -0300

    improve code readability and type validation

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-accumulated = 0
-counter = 0
-average = None
-
+accumulated = counter = average = 0
 people_list = []
+
+
 
 
 def add_value(value):
@@ -17,12 +16,19 @@ def add_value(value):
     accumulated += value
     average = accumulated / counter
 
-
 def people_exist(dni):
     if len(people_list) > 0:
         for ppl in people_list:
             if ppl['dni'] == dni:
                 return ppl
+            else:
+                return False
+    else:
+        return False
+
+def validate_types(name, dni, height):
+    if type(name) == str and type(dni) == int and type(height) == float:
+        return True
     return False
 
 
@@ -35,14 +41,15 @@ def index():
 
 @app.route('/sum', methods=['POST'])
 def sum():
-    value = request.args.get('value')
-    if value is None:
+    value = request.args.get('value', None)
+    if not value:
         return 'You must send the value URL param'
-    value = int(value)
-    if value < 0:
-        return 'Value param must be an integer equal or greater than 0', 400
-    add_value(value)
-    return '', 200
+    else:
+        if int(value) < 0:
+            return 'Value param must be an integer equal or greater than 0', 400
+        else:
+            add_value(int(value))
+            return '', 200
 
 @app.route('/info', methods=['GET'])
 def info():
@@ -56,56 +63,79 @@ def info():
 
 @app.route('/people', methods=['POST', 'GET', 'DELETE'])
 def people():
-    if request.method == 'POST':
 
-        req_data = request.get_json()
-        name = None
-        dni = None
-        height = None
-        if 'name' in req_data and 'dni' in req_data and 'height' in req_data:
-            name = req_data['name']
-            dni = req_data['dni']
-            height = req_data['height']
-            if type(name) == str and type(dni) == int and type(height) == float:
-                if people_exist(dni) is not False:
-                    return 'a person with dni {} already exists in the system'.format(dni), 409
-                else:
-                    people_list.append(req_data)
-                    return jsonify({
-                        "messege": "success"
-                    }), 201
-        return {
+    bad_req_response_expected = {
             "messege": "400 - BAD REQUEST",
             "expected": {
                 "name": "<str value>",
                 "dni": "<int value>",
                 "height": "<float value>" 
             }
-        }, 400
+        }
+
+    if request.method == 'POST':
+
+        req_data = request.get_json(silent=True)
+        if req_data is None:
+            return jsonify(bad_req_response_expected), 400
+
+        name = dni = height = None
+
+        if 'name' in req_data and 'dni' in req_data and 'height' in req_data:
+            name = req_data['name']
+            dni = req_data['dni']
+            height = req_data['height']
+
+            if validate_types(name, dni, height) and not people_exist(dni):
+                people_list.append(req_data)
+                return jsonify({
+                    "messege": "success"
+                }), 201
+            else:
+                if(people_exist(dni)):
+                    return 'a person with dni {} already exists in the system'.format(dni), 409
+                else:
+                    return jsonify(bad_req_response_expected), 400
+        else:
+            return jsonify(bad_req_response_expected), 400
     
 
     if request.method == 'GET':
-        
-        args = request.args.get('dni', None)
-        
-        if args is not None:
-            dni = int(args)
+        dni = request.args.get('dni', None)
+        if dni:
+            try:
+                dni = int(dni)
+            except:
+                return jsonify({
+                    "messege": "expected a <int> value for the key 'dni'"
+                }), 400
             ppl = people_exist(dni)
             if ppl:
                 return jsonify(ppl),200
-            return '', 204
-        
-        return jsonify(people_list), 200
+            else:
+                return '', 204
+        else:
+            return jsonify(people_list), 200
 
     if request.method == 'DELETE':
         dni = request.args.get('dni', None)
         if dni:
-            for ppl in people_list:
-                if ppl['dni'] == int(dni):
-                    people_list.remove(ppl)
-                    return '', 200
-            return 'dni {} not found'.format(dni), 204
-        return '', 400
+            try:
+                dni = int(dni)
+            except:
+                return jsonify({
+                    "messege": "expected a <int> value for the key 'dni'"
+                }), 400
+            founded = [ppl for ppl in people_list if ppl['dni'] == dni]
+            if len(founded) > 0:
+                people_list.remove(founded[0])
+                return '', 200
+            else:
+                return jsonify({
+                    "messege": "dni {} not found".format(dni)
+                }), 204
+        else:
+            return '', 400
 
 if __name__ == '__main__':
     app.run(debug = True)
